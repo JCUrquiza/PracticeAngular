a01438fc5c0fd136de433a7716a850402f30826e
Juan <gurquiza8@gmail.com>
2024-04-20 15:58:37 -0500

    i18nSelect pipe, i didnt know...

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Juan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient(): void {
+    this.name = 'Anel';
+    this.gender = 'female';
+  }
+
 }
