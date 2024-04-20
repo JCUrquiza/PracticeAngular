e1983a3025de55f5e6ff74300720b41d27ce7eb0
Juan <gurquiza8@gmail.com>
2024-04-20 15:19:19 -0500

    lenguaje for pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// Configuración del locale de la app
+import localeEsMx from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsMx);
+
 SharedModule
 
 @NgModule({
