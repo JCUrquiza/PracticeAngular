7d835d9e71825f1f169d8587c0594432e1561bb8
Juan <gurquiza8@gmail.com>
2024-04-22 12:59:27 -0500

    dynamic args for custom pipes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,8 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import { ToolbarModule } from 'primeng/toolbar';
+
 
 @NgModule({
   exports: [
@@ -14,7 +16,8 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
